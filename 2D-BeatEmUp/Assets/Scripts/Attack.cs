adbff44d20f0268a620be9c09854625c5e03164b
trungnt1995 <38932506+trungnt1995@users.noreply.github.com>
2019-10-06 21:24:13 +0700

    Player Movement

---
+++ 2D-BeatEmUp/Assets/Scripts/Attack.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{   
+    public int comboPerforming = 0;
+    private Move[] selectedCombo;
+    public float comboCountinueTime = 0;
+    public Move[] punchCombos;
+
+    public float startComboCooldown;
+    public float startComboCooldownCount;
+    public bool canAttack;
+    public bool isAtacking;
+    public Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        if(startComboCooldownCount > 0)
+        {
+            startComboCooldownCount -= Time.deltaTime;
+        }else
+        {
+            canAttack = true;
+        }
+
+        if(comboCountinueTime > 0)
+        {
+            comboCountinueTime-= Time.deltaTime;
+        }    
+        else if(selectedCombo != null)
+        {
+            EndCombo();                
+        }
+
+        if(Input.GetKeyDown(KeyCode.R) && canAttack)
+        {   if(comboPerforming == 0 && comboCountinueTime <= 0){
+                selectedCombo = punchCombos;
+                StartCombo();
+            }
+            else
+            {
+                comboPerforming++;
+                if(comboPerforming < selectedCombo.Length)
+                {
+                
+                selectedCombo[comboPerforming].Perform();
+                selectedCombo[comboPerforming - 1].Intercrupt();
+                myAnim.SetBool(selectedCombo[comboPerforming].animationName, true);
+                comboCountinueTime = selectedCombo[comboPerforming].duration;
+                }
+            }
+        }
+    }
+
+    public void StartCombo()
+    {   isAtacking = true;
+        selectedCombo[0].Perform();
+        myAnim.SetBool(selectedCombo[0].animationName, true);
+        comboCountinueTime = selectedCombo[0].duration;
+    }
+
+    public void EndCombo()
+    {
+        canAttack = false;
+        isAtacking = false;
+        comboPerforming = 0;
+        startComboCooldownCount = startComboCooldown;
+        for(int i = 0; i < selectedCombo.Length ; i++){
+            myAnim.SetBool(selectedCombo[i].animationName, false);
+        }
+        selectedCombo = null;
+    }
+}
---
+++ 2D-BeatEmUp/Assets/Scripts/Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{   
+    public BoxCollider2D hitBox;
+    public float duration;
+    public float durationCount = 0;
+    public string animationName;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(durationCount > 0)
+        {
+            durationCount -= Time.deltaTime;
+        }else
+        {
+            gameObject.SetActive(false);
+        }
+
+      
+    }
+
+    public void Perform()
+    {   
+        durationCount = duration;
+        gameObject.SetActive(true);
+    }
+
+    public void Intercrupt()
+    {
+        durationCount = 0;
+        gameObject.SetActive(false);
+    }
+}
